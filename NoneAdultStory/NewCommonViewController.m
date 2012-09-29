0edb25d9170e7932fdf7d70b3c576eeecf90e48a
Wang Pan <brianspecial@163.com>
2012-09-29 08:49:05 +0800

    修复广告被遮挡的问题

--- NoneAdultStory/NewCommonViewController.m
+++ NoneAdultStory/NewCommonViewController.m
@@ -92,7 +92,7 @@
 	newFrame.size.height = adSize.height;
 	newFrame.size.width = adSize.width;
 	newFrame.origin.x = (self.view.bounds.size.width - adSize.width)/2;
-    newFrame.origin.y = 480 - adSize.height;
+    newFrame.origin.y = 0;
 	adView.frame = newFrame;
     
 	[UIView commitAnimations];
